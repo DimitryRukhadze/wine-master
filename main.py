e808169ce9344c358c07bea420ccf8ed5693d08d
DimitryRukhadze <lokiinflames@gmail.com>
2022-05-04 21:47:10 +0300

    Refactored main.py
    Refactored get_drinks_from_excel var names
    
    Added environment vars

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import collections
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from os import environ
 
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
+from dotenv import load_dotenv
 
 def count_years_of_work():
 
@@ -18,16 +19,19 @@ def count_years_of_work():
 def get_drinks_from_excel(excel_path):
 
     wine_excel_df = pandas.read_excel(excel_path, keep_default_na=False)
+    wines_from_excel = wine_excel_df.to_dict('records')
     wines_by_category = collections.defaultdict(list)
-    wines = wine_excel_df.to_dict('records')
 
-    for wine in wines:
+    for wine in wines_from_excel:
         wines_by_category[wine['Категория']].append(wine)
 
     return wines_by_category
 
 
 if __name__ == '__main__':
+
+    load_dotenv()
+    excel_file_path = environ.get('EXCEL_FILE_PATH')
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -36,7 +40,7 @@ if __name__ == '__main__':
 
     template = env.get_template('template.html')
 
-    drinks = get_drinks_from_excel('wine3.xlsx')
+    drinks = get_drinks_from_excel(excel_file_path)
 
     rendered_page = template.render(
         years_old=count_years_of_work(),
